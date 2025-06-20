cd604af4f2f8217c5caae0d371d3f7f2d429576a
Casey Turner <7030crt@gmail.com>
2025-06-20 05:52:34 -0600

    Create options.lua

---
+++ nvim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,24 @@
+-- enable mouse mode
+vim.opt.mouse = 'a'
+
+-- enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- case-insensitive searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- tabs & spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- swap file
+vim.opt.swapfile = false
+
+-- history
+vim.opt.history = 1000
+
+-- terminal GUI colors
+vim.opt.termguicolors = true
