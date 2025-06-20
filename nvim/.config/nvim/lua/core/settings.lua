bf99927bc154909bbf6bea65b0217e42d4e4819b
Casey Turner <7030crt@gmail.com>
2025-06-20 14:04:22 -0700

    Create settings.lua

---
+++ nvim/.config/nvim/lua/core/settings.lua
@@ -0,0 +1,30 @@
+local global = vim.g
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.syntax = "on"
+
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+
+vim.opt.mouse = "a"
+
+vim.opt.showmatch = true
+
+vim.opt.ignorecase = true
+
+vim.opt.hlsearch = true
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
