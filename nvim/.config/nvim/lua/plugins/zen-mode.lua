c4cfc0fd46095baaa0946f2e60c21f0a54e869af
Casey Turner <7030crt@gmail.com>
2025-06-25 06:41:41 -0600

    Create zen-mode.lua

---
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,10 @@
+-- https://github.com/folke/zen-mode.nvim
+
+{
+  "folke/zen-mode.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
