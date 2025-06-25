8b99e40dae7daf54a3f9b890f14db34d63cd6cd9
Casey Turner <7030crt@gmail.com>
2025-06-25 06:14:59 -0600

    Update options.lua

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,1 +1,16 @@
 
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+vim.opt.mouse = "a"
+
+vim.opt.showmatch = true
+
+vim.opt.termguicolors = true
