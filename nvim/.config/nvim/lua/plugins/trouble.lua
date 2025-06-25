9e249276f3a7c354d83f2e5ab2071ce5f7b00022
Casey Turner <7030crt@gmail.com>
2025-06-25 06:25:09 -0600

    Create trouble.lua

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,8 @@
+{
+ "folke/trouble.nvim",
+ dependencies = { "nvim-tree/nvim-web-devicons" },
+ opts = {
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+ },
+}
